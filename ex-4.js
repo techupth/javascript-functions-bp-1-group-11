dfa17993739c546f55212203a8396dcaf5c36cb8
Babygungle <apredikanit@gmail.com>
2023-09-29 00:04:39 +0700

    Blue's work

--- ex-4.js
+++ ex-4.js
@@ -1,4 +1,16 @@
 // Exercise #4 Grouping Function with Objects
 
 // Start coding here
-let calculator = {};
+let add = (a, b) => {return a+b};
+let subtract = (a, b) => {return a-b};
+let multiply = (a, b) => {return a*b};
+let divide = (a, b) => {return a/b};
+
+let calculator = {add, subtract, multiply, divide}
+
+let resultAdd = calculator.add(10, 20);
+console.log(resultAdd)
+
+let resultDivide = calculator.divide(3000, 10);
+console.log(resultDivide)
+
