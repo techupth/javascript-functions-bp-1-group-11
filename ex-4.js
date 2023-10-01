8f17f6166b9a6f94714e9a12e3115d0ff58b70e0
Babygungle <144519233+Babygungle@users.noreply.github.com>
2023-10-01 13:04:32 +0700

    Blue's work: fix ex-4

--- ex-4.js
+++ ex-4.js
@@ -1,12 +1,12 @@
 // Exercise #4 Grouping Function with Objects
 
 // Start coding here
-let add = (a, b) => {return a+b};
-let subtract = (a, b) => {return a-b};
-let multiply = (a, b) => {return a*b};
-let divide = (a, b) => {return a/b};
-
-let calculator = {add, subtract, multiply, divide}
+let calculator = {
+    add: function(a, b) {return a+b},
+    subtract: function(a, b) {return a-b},
+    multiply: function(a, b) {return a*b},
+    divide: function(a, b) {return a/b},
+}
 
 let resultAdd = calculator.add(10, 20);
 console.log(resultAdd)
